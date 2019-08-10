3b655f41b5319bf66b3cc11d6529bb59a1adb87d
Michel Palourdio <mpalourdio@gmail.com>
2019-08-10 21:30:08 +0200

    Missing import

--- src/test/java/com/example/demo/MyControllerTest.java
+++ src/test/java/com/example/demo/MyControllerTest.java
@@ -12,6 +12,7 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MyController.class)
+@Import(MyPropertyConfigHolder.class)
 class MyControllerTest {
 
     @Autowired
